4ee073b02cc27a6944ba2bb8b4152f81541d09a1
almir05 <almir.marcelo08@gmail.com>
2021-12-13 13:11:16 -0400

    Respondendo e armazenando perguntas

--- index.js
+++ index.js
@@ -60,4 +60,16 @@ app.get('/pergunta/:id', (req, res) => {
   });
 });
 
+app.post('/responder', (req, res) => {
+  let corpo = req.body.corpo;
+  let perguntaId = req.body.pergunta;
+
+  Resposta.create({
+    corpo,
+    perguntaId
+  }).then(() => {
+    res.redirect(`/pergunta/${perguntaId}`)
+  });
+});
+
 app.listen(8080, () => console.log('App rodando'));
